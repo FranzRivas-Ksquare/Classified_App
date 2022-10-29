59d9d9e6847a193248290b95c9fda04760b43549
Franz Sinisco <sinisco@tutanota.com>
2022-10-29 11:31:48 -0500

    init - first commit

---
+++ lib/router.dart
@@ -0,0 +1,10 @@
+import 'package:classified_app/screens/login_screen.dart';
+import 'package:classified_app/screens/register_screen.dart';
+import 'package:classified_app/screens/home_screen.dart';
+import 'package:classified_app/screens/settings_screen.dart';
+import 'package:classified_app/screens/edit_profile_screen.dart';
+import 'package:classified_app/screens/my_ads_screen.dart';
+import 'package:classified_app/screens/edit_ad_screen.dart';
+import 'package:classified_app/screens/product_detail_screen.dart';
+import 'package:classified_app/screens/create_ad_screen.dart';
+import 'package:classified_app/screens/image_viewer_screen.dart';
