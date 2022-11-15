5a42f5eb9ed82ab0eccc9bb6e46220a3175ce9e9
Franz Sinisco <sinisco@tutanota.com>
2022-11-14 20:36:30 -0600

    Adding to create add screen, services to make it work

--- lib/services/product_ad.services.dart
+++ lib/services/product_ad.services.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:classified_app/model/product_ad.model.dart';
 import 'package:classified_app/services/auth.service.dart';
-import '../utils/constants.dart';
+import 'package:classified_app/utils/constants.dart';
+import 'package:classified_app/utils/alert_manager.dart';
 
 class AdService {
   Future<List<ProductAd>> fetchAdsProducts() async {
@@ -18,15 +19,14 @@ class AdService {
       productsAds =
           adsData.map<ProductAd>((products) => ProductAd.fromJson(products)).toList();
 
-      if (kDebugMode) {
-        print("$productsAds");
-      }
+      kDebugFunc("$productsAds");
+
 
       return productsAds;
     } catch (e) {
-      if (kDebugMode) {
-        print("Error $e");
-      }
+
+      kDebugFunc("Error $e");
+
       return productsAds;
     }
   }
@@ -58,10 +58,37 @@ class AdService {
     var myAdsData = respJson['data'];
     myAds =
         myAdsData.map<ProductAd>((myAd) => ProductAd.fromJson(myAd)).toList();
+    kDebugFunc(myAds);
 
     return myAds;
   }
 
-
-
+  void postAd(context, ProductAd newAd) async {
+    var storage = const FlutterSecureStorage();
+    var url = Uri.parse("$apiUrl/ads");
+    var newAdObj = newAd.toJson();
+    try {
+      var token = await storage.read(key: 'token');
+      kDebugFunc(token);
+      var resp = await http.post(
+        url,
+        body: jsonEncode(newAdObj),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+      kDebugFunc(resp.statusCode);
+      if (resp.statusCode == 401) {
+        var isNewTokenGenerated = await AuthService().refreshToken();
+        if (isNewTokenGenerated) {
+          postAd(context, newAd);
+        }
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      kDebugFunc(e);
+      AlertManager().displaySnackbar(context, newAdObj['message']);
+    }
+  }
 }
--- lib/ui/screens/create_ad_screen.dart
+++ lib/ui/screens/create_ad_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:classified_app/model/product_ad.model.dart';
+import 'package:classified_app/services/product_ad.services.dart';
 
 class CreateAd extends StatelessWidget {
   const CreateAd({super.key});
@@ -17,100 +19,110 @@ class CreateAd extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Create Ad'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-              child: OutlinedButton(
-                onPressed: () {},
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Icon(Icons.photo_camera, color: Color(0xFFF25723),),
-                    Text(
-                      'Upload Photo',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Color(0xFFF25723)),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: 100,
+                margin: const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Icon(Icons.photo_camera, color: Color(0xFFF25723),),
+                      Text(
+                        'Upload Photo',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Color(0xFFF25723)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: TextFormField(
-                controller: _titleCtrl,
-                validator: (value) {
-                  if(value!.isEmpty) {
-                    return 'This is required';
-                  }
-                },
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Title',
-                    prefixIcon: Icon(Icons.title, color: Color(0xFFF25723),)
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: TextFormField(
+                  controller: _titleCtrl,
+                  validator: (value) {
+                    if(value!.isEmpty) {
+                      return 'This is required';
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Title',
+                      prefixIcon: Icon(Icons.title, color: Color(0xFFF25723),)
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: TextFormField(
-                controller: _priceCtrl,
-                validator: (value) {
-                  if(value!.isEmpty) {
-                    return 'This is required';
-                  }
-                },
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Price',
-                    prefixIcon: Icon(Icons.price_change, color: Color(0xFFF25723),)
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: TextFormField(
+                  controller: _priceCtrl,
+                  validator: (value) {
+                    if(value!.isEmpty) {
+                      return 'This is required';
+                    }
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Price',
+                      prefixIcon: Icon(Icons.price_change, color: Color(0xFFF25723),)
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: TextFormField(
-                controller: _cellphoneCtrl,
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Contact',
-                    prefixIcon: Icon(Icons.phone, color: Color(0xFFF25723),)
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: TextFormField(
+                  controller: _cellphoneCtrl,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Contact',
+                      prefixIcon: Icon(Icons.phone, color: Color(0xFFF25723),)
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: TextFormField(
-                controller: _descriptionCtrl,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Description',
-                    prefixIcon: Icon(Icons.description, color: Color(0xFFF25723),)
+              Container(
+                margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                child: TextFormField(
+                  controller: _descriptionCtrl,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Description',
+                      prefixIcon: Icon(Icons.description, color: Color(0xFFF25723),)
+                  ),
                 ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 50,
-              margin: const EdgeInsets.all(10),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFF25723)),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Submit Add'),
+              Container(
+                width: double.infinity,
+                height: 50,
+                margin: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFF25723)),
+                  onPressed: () {
+                    if(_formKey.currentState!.validate()) {
+                      ProductAd newAdPost = ProductAd(
+                        title: _titleCtrl.text,
+                        price: int.tryParse(_priceCtrl.text),
+                        mobile: _cellphoneCtrl.text,
+                        description: _descriptionCtrl.text,
+                        images: ["assets/images/products/switch.jpg"], // TODO: Make image_picker
+                      );
+                      AdService().postAd(context, newAdPost);
+                    }
+                  },
+                  child: const Text('Submit Add'),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-
 }
