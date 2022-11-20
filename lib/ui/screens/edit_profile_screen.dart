001a733051aa6b89e203e0ae209d096c728fd40d
Franz Sinisco <sinisco@tutanota.com>
2022-11-19 18:42:15 -0600

    Adding SingleChildScrollViewe to edit profile

--- lib/ui/screens/edit_profile_screen.dart
+++ lib/ui/screens/edit_profile_screen.dart
@@ -66,98 +66,100 @@ class _EditProfileState extends State<EditProfile> {
       appBar: AppBar(
         title: const Text('Edit Profile'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                captureImageFromGallery();
-              },
-              child: CircleAvatar(
-                radius: 60,
-                backgroundImage: imageSelect(_imagePath),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  captureImageFromGallery();
+                },
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: imageSelect(_imagePath),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
-                    child: TextFormField(
-                      controller: _nameCtrl,
-                      validator: (value) {
-                        if(value!.isEmpty) {
-                          return 'This is required';
-                        }
-                      },
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Name',
-                          prefixIcon: Icon(Icons.account_box)
+              const SizedBox(height: 20),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                      child: TextFormField(
+                        controller: _nameCtrl,
+                        validator: (value) {
+                          if(value!.isEmpty) {
+                            return 'This is required';
+                          }
+                        },
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Name',
+                            prefixIcon: Icon(Icons.account_box)
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    child: TextFormField(
-                      controller: _mailCtrl,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'eMail',
-                          prefixIcon: Icon(Icons.email)
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: TextFormField(
+                        controller: _mailCtrl,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'eMail',
+                            prefixIcon: Icon(Icons.email)
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    child: TextFormField(
-                      controller: _cellphoneCtrl,
-                      keyboardType: TextInputType.phone,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Cellphone',
-                          prefixIcon: Icon(Icons.phone)
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: TextFormField(
+                        controller: _cellphoneCtrl,
+                        keyboardType: TextInputType.phone,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Cellphone',
+                            prefixIcon: Icon(Icons.phone)
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 50,
-                    margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFF25723)),
-                      onPressed: () {
-                        User updateUser = User(
-                          name: _nameCtrl.text,
-                          email: _mailCtrl.text,
-                          mobile: _cellphoneCtrl.text,
-                          image: _imageServerPath.isEmpty
-                              ? 'https://picsum.photos/200'
-                              : _imageServerPath,
-                        );
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFF25723)),
+                        onPressed: () {
+                          User updateUser = User(
+                            name: _nameCtrl.text,
+                            email: _mailCtrl.text,
+                            mobile: _cellphoneCtrl.text,
+                            image: _imageServerPath.isEmpty
+                                ? 'https://picsum.photos/200'
+                                : _imageServerPath,
+                          );
 
-                        kDebugFunc(updateUser);
+                          kDebugFunc(updateUser);
 
-                        UserServices().updateUserInfo(context, updateUser);
-                      },
-                      child: const Text('Update Profile'),
+                          UserServices().updateUserInfo(context, updateUser);
+                        },
+                        child: const Text('Update Profile'),
+                      ),
                     ),
-                  ),
-                  TextButton(onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                    Navigator.pushReplacementNamed(context, '/');
-                  },
-                    child: const Text('Logout', style: TextStyle(color: Color(0xFFF25723)),),
-                  ),
-                ],
-              ),
-            )
-          ],
+                    TextButton(onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                      Navigator.pushReplacementNamed(context, '/');
+                    },
+                      child: const Text('Logout', style: TextStyle(color: Color(0xFFF25723)),),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
